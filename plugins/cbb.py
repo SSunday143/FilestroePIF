f533d21c0da968c078b645cdfabfa8a43b0a235f
SSunday143 <143283610+SSunday143@users.noreply.github.com>
2023-10-05 16:25:31 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Developer : <a href='https://t.me/PIFOficial_Dev'>DevLOVEper 🧑🏻‍💻</a>\n○ Channel : @PanindiaFilmz\n○ Support Group : @BoTzUpdates0</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Developer : <a href='https://t.me/telugufilms4u'>DevLOVEper 🧑🏻‍💻</a>\n○ Channel : @telugufilms4u\n○ Support Group : @telugufilms4u</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
